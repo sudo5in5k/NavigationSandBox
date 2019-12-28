28405eb44e6e4232d5557b694343e115dd82b8d3
ussy <sudo5in5k@gmail.com>
2019-12-28 18:36:14 +0900

    add finish view

--- app/src/main/java/com/example/navigationsandbox/FinishFragment.kt
+++ app/src/main/java/com/example/navigationsandbox/FinishFragment.kt
@@ -6,17 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_result.view.*
+import kotlinx.android.synthetic.main.fragment_finish.view.*
 
-class ResultFragment: Fragment() {
+class FinishFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_result, container, false)
+        val view = inflater.inflate(R.layout.fragment_finish, container, false)
         view.button.setOnClickListener {
-            findNavController().navigate(R.id.action_to_finish)
+            findNavController().navigate(R.id.action_to_first)
         }
         return view
     }
