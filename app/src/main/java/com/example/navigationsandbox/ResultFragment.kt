f9fdcf567980346319bca4204dfbfb8f7d78f52e
ussy <sudo5in5k@gmail.com>
2019-12-28 17:56:56 +0900

    fitst

---
+++ app/src/main/java/com/example/navigationsandbox/ResultFragment.kt
@@ -0,0 +1,23 @@
+package com.example.navigationsandbox
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_result.view.*
+
+class ResultFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_result, container, false)
+        view.button.setOnClickListener {
+            findNavController().navigate(R.id.action_to_first)
+        }
+        return view
+    }
+}
